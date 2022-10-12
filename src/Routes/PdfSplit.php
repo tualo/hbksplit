a3380440a986153c67a02bf007da67e87aa9b4d5
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-10-12 13:01:21 +0200

    test upload

--- src/Routes/PdfSplit.php
+++ src/Routes/PdfSplit.php
@@ -18,6 +18,7 @@ class PdfSplit implements IRoute{
                 ini_set('upload_max_filesize','2GB');
                 ini_set('post_max_size','2GB');
                 
+
                 $newfile = PDF_SPLIT_PATH.'/'.$jobid.'pdf';
                 if (isset($_FILES['userfile'])){
                     $sfile = $_FILES['userfile']['tmp_name'];
@@ -31,6 +32,48 @@ class PdfSplit implements IRoute{
                       unlink($newfile);
                     }
                   }
+                }else{
+                    if (!isset($_SERVER['HTTP_X_FILE_NAME'])) throw new \Exception("Unknown file name");
+                    $fileName = $_SERVER['HTTP_X_FILE_NAME'];
+                    if (isset($_SERVER['HTTP_X_FILENAME_ENCODER']) && 'base64' == $_SERVER['HTTP_X_FILENAME_ENCODER']) {
+                        $fileName = base64_decode($fileName);
+                    }
+                    $fileName = htmlspecialchars($fileName);
+                    $mimeType = htmlspecialchars($_SERVER['HTTP_X_FILE_TYPE']);
+                    $size = intval($_SERVER['HTTP_X_FILE_SIZE']);
+                    $inputStream = fopen('php://input', 'r');
+                    $outputFilename = PDF_SPLIT_PATH . '/' . $fileName;
+                    $realSize = 0;
+                    $data = '';
+                    if ($inputStream) {
+                        if (! $config['fake']) {
+                            $outputStream = fopen($outputFilename, 'w');
+                            if (! $outputStream) {
+                                throw new \Exception( 'Error creating local file');
+                            }
+                        }
+
+                        while (! feof($inputStream)) {
+                            $bytesWritten = 0;
+                            $data = fread($inputStream, 1024);
+
+                            $bytesWritten = fwrite($outputStream, $data);
+
+                            if (false === $bytesWritten) {
+                                throw new \Exception('Error writing data to file');
+                            }
+                            $realSize += $bytesWritten;
+                        }
+
+                        fclose($outputStream);
+
+                    } else {
+                        throw new \Exception('Error reading input');
+                    }
+                    $tmp_name = $outputFilename;
+                    if ($realSize != $size) {
+                        throw new \Exception('The actual size differs from the declared size in the headers');
+                    }
                 }
 
                 App::result('success',true);
